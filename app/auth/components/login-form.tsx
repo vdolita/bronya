3d9dd77fd50d4dc8d4342821b250022854861467
Vdolita <vdolita@outlook.com>
2023-12-26 12:02:03 +0800

    install shadcn ui

---
+++ app/auth/components/login-form.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { Button } from "@/sdui/ui/button";
+
+const LoginForm = () => {
+
+  return (
+    <div className="w-96 h-fit">
+      
+          <Button className="w-full" type="submit">
+            Submit
+          </Button>
+
+      <div className="h-40" />
+    </div>
+  );
+};
+
+export default LoginForm;
---
+++ app/auth/page.tsx
@@ -0,0 +1,9 @@
+import LoginForm from "./components/login-form";
+
+export default function AuthPage() {
+  return (
+    <div className="h-full w-full flex justify-center items-center">
+      <LoginForm />
+    </div>
+  );
+}
